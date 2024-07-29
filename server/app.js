6de092cdd95f90d3ed6e5315280a4a66c6150cde
unknown <techcode057@gmail.com>
2024-07-29 23:55:09 +0545

    auth-completed

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
  import express from "express";
+ import cookieParser from "cookie-parser"
 import AuthRoute from "./routes/auth.route.js"
 
  const app =  express();
 
 
 
- app.use(express.json())
+ app.use(express.json());
+ app.use(cookieParser());
 
  app.listen(8800 , () => {
     console.log(`server is running `);
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,43 +1,101 @@
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
 
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
 
-export  const register = async (req, res) => {
-    const {username , email , password} =  req.body
- 
-    if (!username || !email || !password) {
-      return   res.status(400).json({
-            message: 'Please provide all required fields'
-        })
-            
-        
-        
+  // verfiying placeholder
+  if (!username || !email || !password) {
+    return res.status(400).json({
+      message: "Please provide all required fields",
+    });
+  }
+
+  try {
+    // password hashing
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // creating new user on DB
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+    });
+
+    res.status(200).json({
+      message: "User Register Successfully",
+      newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to create user",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({
+      message: "please provide required field",
+    });
+  }
+
+  try {
+    //check user exist
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "Invalid Credentials !",
+      });
     }
 
-    try {
+    //check if the password is correct
 
-        const hashedPassword =  await bcrypt.hash(password , 10);
-        console.log(hashedPassword);
-        
-    } catch (error) {
-        return   res.status(500).json({
-            message: 'internal server error'
-        })
-        
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(404).json({
+        message: "Invalid Credentials !",
+      });
     }
 
+    const age = 1000 * 60 * 60 * 24 * 7;
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
 
+    //generate COOKIE Tokn and send to the user
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        maxAge: age,
+      })
+      .status(200)
+      .json({
+        message: "Login Successfully",
+      });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Failed to login",
+    });
+  }
+};
 
-
-
-
-}
-
-export  const login = (req, res) => {
-
-    
-}
-
-export  const logout = (req, res) => {
-
-    
-}
+export const logout = async (req, res) => {
+  res.clearCookie("token").status(200).json({
+    message:"Logout Sucessfully"
+  })
+};
